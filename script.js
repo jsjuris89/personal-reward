e80911ca171dc5e75e922f072560ebb7af15a027
Juris Poselis <jsjuris89@gmail.com>
2024-02-14 18:36:54 +0200

    created function calculateTotalPoints()

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 let actionValues = {
-    eated: "",
-    eatedValue: ""
+    eatInMorning: "",
+    eatInMorningPoints: "",
+    overEating: "",
+    overEatingPoints: ""
 }
 
 function addUserInput() {
-    let hasEated = document.getElementById("task1").checked;
-    let hasEatedPoints = document.getElementById("eatedValue").value
+    let hasEated = document.getElementById("eatInMorning").checked;
+    let hasEatedPoints = document.getElementById("eatInMorningPoints").value
+
+    let overeating = document.getElementById("noOverEating").checked;
+    let overeatingPoints = document.getElementById("noOverEatingPoints").value;
+
     
-    actionValues['eated'] = hasEated;
-    actionValues['eatedValue'] = hasEatedPoints;
+    actionValues['eatInMorning'] = hasEated;
+    actionValues['eatInMorningPoints'] = hasEatedPoints;
+
+    actionValues['overEating'] = overeating;
+    actionValues['overEatingPoints'] = overeatingPoints;
 }
 addUserInput();
 
 
+// function to calculate total points
+let totalPoints = 0;
+function calculateTotalPoints() {
+    for (let key in actionValues) {
+        if (key.endsWith('Points')) {
+            totalPoints += parseInt(actionValues[key]);
+        }
+    }
+    return totalPoints;
+}
 
 
